bd4477fc1cf52482d92d2488c34b1bc3c37a140f
Cristian Gomez <132181160+Cristian-Gomez12@users.noreply.github.com>
2024-12-14 02:06:44 -0800

    Add files via upload

--- dynamic_programming_method/DP.py
+++ dynamic_programming_method/DP.py
@@ -1,43 +1,30 @@
+import random
+
 def soccer_dyn_prog(grid):
-    # Get the number of rows and columns
     row, col = len(grid), len(grid[0])
-
-    # Corner case: initial cell is impassible
-    if grid[0][0] == 'X' or grid[row - 1][col - 1] == 'X':
+    if grid[0][0] == 'X' or grid[row-1][col-1] == 'X':
         return 0
-
-    # Initialize the DP grid with zeroes
     dp = [[0 for _ in range(col)] for _ in range(row)]
-
-    # Base case
     dp[0][0] = 1
     for i in range(row):
         for j in range(col):
-            if grid[i][j] == 'X':  
+            if grid[i][j] == 'X':
                 dp[i][j] = 0
                 continue
-
-            # From above
             if i > 0:
-                dp[i][j] += dp[i - 1][j]
-
-            # From the left
+                dp[i][j] += dp[i-1][j]
             if j > 0:
-                dp[i][j] += dp[i][j - 1]
+                dp[i][j] += dp[i][j-1]
+    return dp[row-1][col-1]
 
-    return dp[row - 1][col - 1]
+def generate_grid(n, blocked_ratio=0.2): # 0.2 = 20% of the grid will be an X (blocked)
+    grid = [['.' for _ in range(n)] for _ in range(n)] # grid of .'s
+    num_blocks = int(n * n * blocked_ratio)
+    for _ in range(num_blocks):
+        x, y = random.randint(0, n-1), random.randint(0, n-1) #place a random block in the grid
+        grid[x][y] = 'X'
+    grid[0][0] = '.'  
+    grid[n-1][n-1] = '.' #make sure start and finish are open
+    return grid
 
 
-grid_example = [
-    ['.', '.', '.', '.', '.', '.', 'X', '.', 'X'],
-    ['X', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', 'X', '.', '.', '.', 'X', '.'],
-    ['.', '.', 'X', '.', '.', '.', '.', 'X', '.'],
-    ['.', 'X', '.', '.', '.', '.', 'X', '.', '.'],
-    ['.', '.', '.', '.', 'X', '.', '.', '.', '.'],
-    ['.', '.', 'X', '.', '.', '.', '.', '.', 'X'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.']
-]
-
-ans = soccer_dyn_prog(grid_example)
-print(f"Number of different paths: {ans}")
